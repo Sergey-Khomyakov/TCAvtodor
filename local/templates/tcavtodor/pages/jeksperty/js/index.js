a75462cef7853bed2fd6a19dcff58b8aabbb8904
Sergey-Khomyakov <ser.xomiakoff2011@yandex.ru>
2025-01-24 16:58:23 +0300

    change fonts and fix all page

--- local/templates/tcavtodor/pages/jeksperty/js/index.js
+++ local/templates/tcavtodor/pages/jeksperty/js/index.js
@@ -1,9 +1,11 @@
 import dialogMeneger from './../../../modules/dialogMeneger.mjs'
+import tabs from './../../../modules/tabs.mjs'
 //import accordion from './../../../modules/accordion.mjs'
 //import select from './../../../modules/select.mjs'
 
 $(function() { 
     dialogMeneger();
+    tabs();
     // accordion();
     // select();
 
--- local/templates/tcavtodor/pages/partneram/js/index.js
+++ local/templates/tcavtodor/pages/partneram/js/index.js
@@ -1,10 +1,43 @@
 import dialogMeneger from './../../../modules/dialogMeneger.mjs'
-//import accordion from './../../../modules/accordion.mjs'
-//import select from './../../../modules/select.mjs'
+import { preloadImages } from './../../../libs/gsap/utils.js';
 
 $(function() { 
     dialogMeneger();
+
+    scroll();
     // accordion();
     // select();
 
 });
+
+
+// // Function to handle scroll-triggered animations
+// const scroll = () => {
+//     const content = document.querySelector('.wrap');
+//     const contentElements = document.querySelectorAll('.content--sticky');
+    
+//     contentElements.forEach((el, index) => {
+//         if (index === 0) {
+//             gsap.to(el, {
+//                 scrollTrigger: {
+//                     trigger: content,
+//                     start: "top top",
+//                     end: "bottom top",
+//                     scrub: true,
+//                     pin: true, // Фиксация первой карточки
+//                 }
+//             });
+//         } else {
+//             gsap.fromTo(el, {y: '0px'}, {
+//                 y: `-${el.offsetHeight}px`, // Двигаем элементы по оси Y
+//                 scrollTrigger: {
+//                     trigger: content,
+//                     start: "top top",
+//                     end: "bottom bottom",
+//                     pin: true,
+//                     scrub: true,
+//                 }
+//             });
+//         }
+//     });
+// };
